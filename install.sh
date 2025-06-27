159b01de44629b36deed8cbf55c264647b9b2b96
Tom Trunko <tom@trunko.dev>
2025-06-27 18:28:21 -0500

    Add install script and modify waybar and wofi style

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Install necessary packages
 yay -S hyprland hyprpaper hyprshot swaync wofi waybar yazi dolphin firefox xdg-desktop-portal-hyprland hyprpolkitagent starship htop neofetch lazygit ttf-hack-nerd
+
+
