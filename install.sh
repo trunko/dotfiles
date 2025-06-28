ccb85099084b152d1ddae2526253e3100b45acb3
Tom Trunko <tom@trunko.dev>
2025-06-28 14:23:40 -0500

    Move app launcher to left side and add window title to center

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install necessary packages
-while read i; do yay -S --noconfirm $i; done <~/dotfiles/packages.txt
+while read -r i; do yay -S --noconfirm "$i"; done <~/dotfiles/packages.txt
 
 # Create Symlinks
 ln -s ~/dotfiles/config/* ~/.config/
